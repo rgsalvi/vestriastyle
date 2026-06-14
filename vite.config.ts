83cdda2ded51e23545d94ccd6100a92fbb6b5aa7
Ram Salvi <rgsalvi84@gmail.com>
2026-06-14 15:10:19 +0530

    fix: Add vite-plugin-wasm to handle TensorFlow WASM modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import wasm from 'vite-plugin-wasm'
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [wasm(), react()],
   build: {
     chunkSizeWarningLimit: 3000,
     rollupOptions: {
